1745c98f6a50892fe99584ee6d92d2d7980e1489
cpenaper <71782814+cpenaper@users.noreply.github.com>
2021-08-24 12:12:42 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!-- INICIO CONTENIDO PRINCIPAL -->
+
+<div class="container">
+    <h1>HISTORICO DE BOLETAS... AQUI VA EL GRAFICO </h1>
+</div>
+
+<!-- FIN CONTENIDO PRINCIPAL -->
+
+
+<?php require_once "vistas/parte_inferior.php"?>
+
---
+++ registrar.php
@@ -0,0 +1,12 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!-- INICIO CONTENIDO PRINCIPAL -->
+
+<div class="container">
+    <h1>REGISTRO DE BOLETERIA</h1>
+</div>
+
+<!-- FIN CONTENIDO PRINCIPAL -->
+
+
+<?php require_once "vistas/parte_inferior.php"?>
